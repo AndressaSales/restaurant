3e588d695da2921bf02fea7d9dbbc707bc9ba1e2
AndressaSales <andressaccs2001@gmail.com>
2025-01-28 15:59:43 -0300

    finalizando o component gallery

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -5,19 +5,16 @@ import GalleryCarrosel from './GalleryCarrosel'
 
 const Gallery = () => {
   return (
-    <div className=' bg-primaryblack justify-center items-center flex flex-row' style={{padding: '4rem 0 4rem 6rem'}}>
-      <div className='flex-[1] flex justify-center items-start flex-col min-w-[500px] pr-[2rem]'>
+    <div className=' bg-primaryblack justify-center items-center flex lg:flex-row md:flex-col sm:flex-col' style={{padding: '4rem 0 4rem 6rem'}}>
+      <div className='flex justify-center lg:items-start sm:items-center flex-col min-w-[700px] pr-[2rem]'>
         <SubHeading title='Instagram' />
-        <h1 className='capitaliza font-serif text-golden tracking-tighte-[0.04em] lg:text-5xl md:text-3xl sm:text-2xl'>Fotos da Galeria</h1>
-        <p className='font-mono font-normal capitalize' style={{color: '#aaa', margin: '2rem 0'}}>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam est laudantium consectetur quia quos ea officiis qui ullam, corrupti repellendus illo dicta impedit in vero voluptates, accusantium debitis aut dolore.
+        <h1 className='capitaliza font-serif text-golden tracking-tighte-[0.04em] lg:text-5xl md:text-3xl sm:text-2xl pb-6'>Fotos da Galeria</h1>
+        <p className='font-mono font-normal capitalize text-grey ' style={{margin: '2rem 0'}}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ut officia repellendus. Accusamus maxime cupiditate, quae maiores ullam ipsum! Obcaecati rem est rerum consequatur perferendis! Inventore quam unde cupiditate officia.
         </p>
-        <button className='bg-crismson text-primaryblack text-[16px]tracking-[0.04em] font-bold leading-[28px] outline-none' style={{padding: '0.5rem 1.5rem'}}>Ver Mais</button>
       </div>
-      <div className='flex flex-[1] flex-row max-w-[50%] relative'>
-          <div className=''>
-              <GalleryCarrosel />
-          </div>
+      <div>
+        <GalleryCarrosel />
       </div>
     </div>
   )
--- src/components/gallery/GalleryCarrosel.jsx
+++ src/components/gallery/GalleryCarrosel.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import images from '../../utils/images'
+import { motion } from 'framer-motion'
 
 const GalleryCarrosel = () => {
 
-      const galletyImgens = [images.gallery01, images.gallery02, images.gallery03, images.gallery04]
+  const carrosel = useRef()
+  const [width, setWidth] =  useState(0)
+
+  useEffect(() => {
+    console.log(carrosel.current?.scrollWidth, carrosel.current?.offsetWidth)
+    setWidth(carrosel.current?.scrollWidth - carrosel.current?.offsetWidth)
+  }, [])
+
+  const galletyImgens = [images.gallery01, images.gallery02, images.gallery03, images.gallery04]
 
   return (
-    <div>
-      GalleryCarrosel
+    <div ref={carrosel} className='flex items-center justify-center lg:max-w-[650px] md:max-w-[650px] sm:max-w-[300px]' style={{margin: '0 auto'}}>
+      <motion.div className='cursor-grab overflow-hidden'>
+        <motion.div 
+          className='flex'
+          whileTap={{cursor: 'grabbing'}}
+          drag='x'
+          dragConstraints={{right: 0, left: -width}}
+          initial={{x: 100}}
+          animate={{x: 0}}
+          transition={{duration: 0.8}}
+        >
+          {
+            galletyImgens.map((img) => {
+              return(
+                <motion.div key={img} className='min-h-[447px] min-w-[301px] p-[14px]  '>
+                  <img src={img} alt="images" className='w-full h-[90%] object-cover rounded-[12px] pointer-events-none' />
+                </motion.div>
+              )
+            })
+          }
+        </motion.div>
+      </motion.div>
     </div>
   )
 }
