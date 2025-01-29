b8815c3401872491144927c8953f3c99681bf041
AndressaSales <andressaccs2001@gmail.com>
2025-01-29 18:18:33 -0300

    ajeitando o component intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,17 +3,19 @@ import meal from '../../assets/meal.mp4'
 
 const Intro = () => {
   return (
-    <div className='relative flex justify-center items-center w-[100%] h-[450px]'>
-        <video autoPlay loop playsInline muted className='absolute w-[100%] h-[450px]  object-cover right-0 bottom-0 z-[-1]'>
+    <div className='bg-primaryblack py-24'>
+      <div className='relative max-w-7xl mx-auto h-[550px] md:h-[600px]'>
+        <video autoPlay loop playsInline muted className='absolute w-[100%] h-[550px] md:[600px] object-cover right-0 bottom-0 z-[1] md:rounded-2xl'>
             <source src={meal} type='video/mp4' />
         </video>
         <div>
-        <div className='bg-primaryblack w-[100%] h-[450px] absolute items-center justify-center flex inset-0 bg-opacity-50 text-center'>
-          <div className='text-center'>
-            <h1 className='lg:text-3xl text-xl text-golden tracking-[0.04em] font-serif font-bold'>Desvende os Segredos da Culinária</h1>
+          <div className='bg-primaryblack md:rounded-2xl absolute items-center justify-center flex inset-0 bg-opacity-50 text-center z-10'>
+            <div className='text-center'>
+              <h1 className='lg:text-4xl text-2xl text-golden tracking-[0.04em] font-serif font-bold'>Desvende os Segredos da Culinária</h1>
+            </div>
           </div>
         </div>
-        </div>
+      </div>
     </div>
   )
 }
