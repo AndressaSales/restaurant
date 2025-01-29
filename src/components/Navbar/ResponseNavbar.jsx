7ad5b50e6f203452849a3d49fb3387231592fe84
AndressaSales <andressaccs2001@gmail.com>
2025-01-29 17:37:11 -0300

    atualizando a navbar

--- src/components/Navbar/ResponseNavbar.jsx
+++ src/components/Navbar/ResponseNavbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ResponseNavbar = ({menu, setMenu}) => {
   return (
-    <div className={`${menu ? "left-0" : "left-[100%]"} fixed bottom-0 top-0 flex h-screen w-[75%] flex-col justify-center bg-[#0C0C0C] px-8 pb-16 text-[#DCCA87]`}>
+    <div className={`${menu ? "left-0" : "left-[100%]"} fixed bottom-0 top-0 flex h-screen w-[75%] flex-col justify-center bg-[#0C0C0C] px-8 pb-16 text-[#DCCA87] z-10`}>
       <div>
         <ul className='flex flex-col space-y-4 text-xl text-center'>
           <li onClick={() => setMenu(false)}><a href="#home">Home</a></li>
