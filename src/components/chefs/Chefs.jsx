2d1911794d3b682b8a10b39550431c0c8b026481
AndressaSales <andressaccs2001@gmail.com>
2025-01-31 14:28:02 -0300

    atualizando a components chefs

--- src/components/chefs/Chefs.jsx
+++ src/components/chefs/Chefs.jsx
@@ -6,22 +6,22 @@ const Chefs = () => {
   return (
     <div className='bg-primaryblack h-[100vh] w-[100%] justify-center items-center lg:flex-row md:flex-row flex flex-col'>
         <div className='flex-1 w-[100%] flex justify-center items-center ml-[2rem] '>
-            <img src={images.chef} alt="chef" className='w-[50%]' />
+            <img src={images.chef} alt="chef" className='lg:w-[50%] md:w-[50%] w-[300px]' />
         </div>
-        <div className='flex-1 w-[100%] flex justify-center items-start flex-col container'>
+        <div className='flex-[1] w-[100%] flex justify-center items-start flex-col container'>
             <SubHeading title='Palavras do Chef' />
-            <h1 className='text-golden tracking-[0.04em] uppercase leading-[117px] text-5xl md:text-3xl sm:text-base font-serif'>No Que Acreditamos</h1>
+            <h1 className='text-golden tracking-[0.04em] uppercase leading-[117px] lg:text-5xl md:text-3xl sm:text-base font-serif'>No Que Acreditamos</h1>
             <div className='text-white'>
                 <div className='flex justify-start items-end mt-7'>
-                    <img src={images.quote} style={{margin: '0 1rem 1rem 0', width: '47px', height: '40px'}} />
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+                    <img src={images.quote} style={{margin: '0 1rem 1rem 0'}} className='lg:w-[47px]sm:w-[30px] h-[40px] sm:h-[20px]' />
+                    <p className='text-base'>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
                 </div>
-                <p>Autem asperiores sed accusantium, quo possimus amet corrupti neque, odio animi ipsa assumenda illum ad corporis debitis laborum unde, velit sit deleniti?</p>
+                <p className='text-base'>Autem asperiores sed accusantium, quo possimus amet corrupti neque, odio animi ipsa assumenda illum ad corporis debitis laborum unde, velit sit deleniti?</p>
             </div>
-            <div className='w-[100%] mt-[3rem]'>
+            <div className='w-[100%] mt-[3rem] '>
                 <p className='text-golden'>Kevin Luo</p>
                 <p className='text-grey font-normal capitalize  mb-7'>Chef & Fundador</p>
-                <img src={images.sign} className='w-[300px]' />
+                <img src={images.sign} className='lg:w-[300px] md:w-[300px] w-[200px]' />
             </div>
         </div>
     </div>
