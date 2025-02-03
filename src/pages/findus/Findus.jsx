db88f3aaace42710c978a9289a19c169ce8c5edc
AndressaSales <andressaccs2001@gmail.com>
2025-02-03 14:42:50 -0300

    finalizando a page findus

--- src/pages/findus/Findus.jsx
+++ src/pages/findus/Findus.jsx
@@ -5,9 +5,9 @@ import SubHeading from '../../components/SubHeading/SubHeading'
 const Findus = () => {
   return (
     <div id='contact' className='bg-primaryblack w-full h-[100vh] justify-between items-center lg:flex-row flex md:flex-row sm:flex-col'>
-        <div className='lg:pl-72 sm:pl-0 container md:pl-10'>
+        <div className=' w-[100%] flex-[1] flex justify-center items-center  lg:pl-72 flex-col'>
           <SubHeading title='Contatos' />
-          <h1 className='text-golden tracking-[0.04em] capitalize text-5xl md;3xl sm:text-2xl leading-[117px] font-serif' style={{marginBottom: '3rem'}}>Encontre-nos</h1>
+          <h1 className='text-golden tracking-[0.04em] capitalize text-4xl md:text-3xl sm:text-2xl leading-[117px] font-serif' style={{marginBottom: '3rem'}}>Encontre-nos</h1>
           <div>
             <p className='text-grey'>Rua Das Calçadas, n° 2025 </p>
             <p className='' style={{color: '#dcca87', margin: '2rem 0'}}>Horário de funcionamento</p>
@@ -17,7 +17,7 @@ const Findus = () => {
         </div>
         <div className='justify-center items-center flex
         pt-6 pr-7'>
-          <img src={images.findus} className='lg:w-[60%] md:w-[50%] sm:w-[200px]' />
+          <img src={images.findus} className='lg:w-[40%] sm:w-[200px]' />
         </div>
     </div>
   )
